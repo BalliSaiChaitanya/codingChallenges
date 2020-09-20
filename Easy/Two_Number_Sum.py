0caf15a005ff4ec67cf9d24f9fefdbaf0fc3e6bf
BalliSaiChaitanya <schaitanya.balli@gmail.com>
2020-09-20 15:14:39 +0530

    nth much

--- Easy/Two_Number_Sum.py
+++ Easy/Two_Number_Sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hMap={}
-        for i,j in enumerate(nums):
-            diff=target-j
+        for i in range(0,len(nums)):
+            diff=target-nums[i]
             if diff in hMap:
                 return [hMap[diff],i]
-            hMap[j]=i
+            hMap[nums[i]]=i
         return
